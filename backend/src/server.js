7732a8172625f26257bb36448b6e935b0428aa3c
Antiosek <jacentartur@gmail.com>
2026-01-20 22:55:34 +0100

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
 
 dotenv.config();
 // const express = require("express");
@@ -13,13 +13,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors(
-  {
-    origin: {"http://localhost:5173/": true},
-  }
-));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: { "http://localhost:5173/": true },
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
+const __dirname = path.resolve();
 
 // app.use((req, res, next) => {
 //   console.log(`Req method is ${req.method} & Req Url is ${req.url}`);
@@ -28,6 +31,12 @@ app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
